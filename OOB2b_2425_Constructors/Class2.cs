c492bf2f1a88641a83a876e8035382856a017d57
Julian Laspona <JulianDevAcc@hotmail.ph>
2024-11-07 11:31:43 +0800

    Add project files.

---
+++ OOB2b_2425_Constructors/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOB2b_2425_Constructors
+{
+    internal class Class2
+    {
+        private int a = 5;
+
+        public Class2()
+        {
+            DisplayA();
+        }
+
+        public Class2(int val)
+        {
+            a = val;
+        }
+
+        public void DisplayA()
+        {
+            Console.WriteLine($"The value of a is {a}");
+        }
+    }
+}
---
+++ OOB2b_2425_Constructors/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOB2b_2425_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+            Class2 b = new Class2(55);
+            Class2 c = new Class2();
+
+            //a.DisplayA();
+            //a.a = 45;
+            //a.DisplayA();
+
+            b.DisplayA();
+
+            Console.ReadKey();
+
+        }
+    }
+}
